6acebcf559a411149dc4418365ac72b5770aefb3
kiiroisenko786 <projectyuni5@gmail.com>
2024-04-12 00:18:53 +0100

    small update

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+brands = [
+    {
+        "name": "Adidas",
+        "items": [
+            {
+                "name": "Adidas Predator Pro PROMO Fingersave",
+                "price": 117.00
+            }
+        ]
+    }
+]
+
+# Flask Route
+# First part is endpoint
+@app.get("/brands")
+def get_brands():
+    return {"brands" : brands}
