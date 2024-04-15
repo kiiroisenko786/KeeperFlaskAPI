0664a2348fa5c69f9a577c5d28cf31ffd3ad97e1
kiiroisenko786 <projectyuni5@gmail.com>
2024-04-15 19:00:05 +0100

    add get specific brand and specific brand item

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,14 +16,42 @@ brands = [
 
 # Flask Route
 # First part is endpoint
+# Returns all brands as well as their items
 @app.get("/brand")
 def get_brands():
     return {"brands" : brands}
 
+# Creates new brand
 @app.post("/brand")
 def create_brand():
-    pass
+    request_data = request.get_json()
+    new_brand = {"name": request_data["name"], "items": []}
+    brands.append(new_brand)
+    return new_brand, 201
 
+# Creates a new item for a specific brand
 @app.post("/brand/<string:name>/item")
 def create_item(name):
-    pass
+    request_data = request.get_json()
+    for brand in brands:
+        if brand["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            brand["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Brand not found"}, 404
+
+# Retuns a specific brand
+@app.get("/brand/<string:name>")
+def get_brand(name):
+    for brand in brands:
+        if brand["name"] == name:
+            return brand
+    return {"message": "Brad not found"}, 404
+
+# Returns a specific item from a specific brand
+@app.get("/brand/<string:name>/item")
+def get_item_in_brand(name):
+    for brand in brands:
+        if brand["name"] == name:
+            return {"items": brand["items"]}
+    return {"message": "Brand not found"}, 404
