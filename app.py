4af0d0044ec3ac30f4d753b10606fe8d302cb0f2
Taahaa <projectyuni5@gmail.com>
2024-04-22 03:30:50 +0100

    add flaskenv, db.py, update endpoints to dicts

--- app.py
+++ app.py
@@ -1,57 +1,54 @@
+import uuid
 from flask import Flask, request
+from db import items, brands
 
 app = Flask(__name__)
 
-brands = [
-    {
-        "name": "Adidas",
-        "items": [
-            {
-                "name": "Adidas Predator Pro PROMO Fingersave",
-                "price": 117.00
-            }
-        ]
-    }
-]
-
 # Flask Route
 # First part is endpoint
-# Returns all brands as well as their items
+
+# Returns all brands
 @app.get("/brand")
 def get_brands():
-    return {"brands" : brands}
+    return {"brands" : list(brands.values())}
+
+# Returns all items
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 # Creates new brand
 @app.post("/brand")
 def create_brand():
-    request_data = request.get_json()
-    new_brand = {"name": request_data["name"], "items": []}
-    brands.append(new_brand)
+    brands_data = request.get_json()
+    brand_id = uuid.uuid4().hex
+    new_brand = {**brands_data, "id": brand_id}
+    brands[brand_id] = new_brand
     return new_brand, 201
 
 # Creates a new item for a specific brand
-@app.post("/brand/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for brand in brands:
-        if brand["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            brand["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Brand not found"}, 404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["brand_id"] not in brands:
+        return {"message": "Brand not found"}, 404
+    item_id = uuid.uuid4.hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
 
 # Retuns a specific brand
 @app.get("/brand/<string:name>")
-def get_brand(name):
-    for brand in brands:
-        if brand["name"] == name:
-            return brand
-    return {"message": "Brad not found"}, 404
+def get_brand(brand_id):
+    try:
+        return brands[brand_id]
+    except KeyError:
+        return {"message": "Brad not found"}, 404
 
-# Returns a specific item from a specific brand
-@app.get("/brand/<string:name>/item")
-def get_item_in_brand(name):
-    for brand in brands:
-        if brand["name"] == name:
-            return {"items": brand["items"]}
-    return {"message": "Brand not found"}, 404
+# Returns a specific item
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Brand not found"}, 404
---
+++ db.py
@@ -0,0 +1,11 @@
+brands = {}
+items = {
+    1 : {
+        "name": "Adidas Predator PRO",
+        "price": 110.00
+    },
+    2 : {
+        "name": "Adidas Copa PRO",
+        "price": 100.00
+    }
+}
