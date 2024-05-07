821bb7d91dabb734221944e9ff6dc7cd35ebcc7a
Taahaa <projectyuni5@gmail.com>
2024-05-07 03:29:37 +0100

    add update and delete, change brand to retailer

--- app.py
+++ app.py
@@ -1,75 +1,91 @@
 import uuid
 from flask import Flask, request
 from flask_smorest import abort
-from db import items, brands
+from db import items, retailers
 
 app = Flask(__name__)
 
 # Flask Route
 # First part is endpoint
 
-# Returns all brands
-@app.get("/brand")
-def get_brands():
-    return {"brands" : list(brands.values())}
+# Returns all retailers
+@app.get("/retailer")
+def get_retailers():
+    return {"retailers" : list(retailers.values())}
 
 # Returns all items
 @app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}
 
-# Creates new brand
-@app.post("/brand")
-def create_brand():
-    brands_data = request.get_json()
-    if "name" not in brands_data:
+# Creates new retailer
+@app.post("/retailer")
+def create_retailer():
+    retailers_data = request.get_json()
+    if "name" not in retailers_data:
         abort(
             400,
             message="Bad request"
         )
-    for brand in brands.values():
-        if brands_data["name"] == brand["name"]:
-            abort(400, message=f"Brand already exists")
-    brand_id = uuid.uuid4().hex
-    new_brand = {**brands_data, "id": brand_id}
-    brands[brand_id] = new_brand
-    return new_brand, 201
+    for retailer in retailers.values():
+        if retailers_data["name"] == retailer["name"]:
+            abort(400, message=f"retailer already exists")
+    retailer_id = uuid.uuid4().hex
+    new_retailer = {**retailers_data, "id": retailer_id}
+    retailers[retailer_id] = new_retailer
+    return new_retailer, 201
 
-# Creates a new item for a specific brand
+# Creates a new item for a specific retailer
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
     if (
         "price" not in item_data
-        or "brand_id" not in item_data
+        or "retailer_id" not in item_data
         or "name" not in item_data
     ):
         abort(
             400,
-            message = "Bad request; Ensure 'price', 'brand_id' and 'name' are included in the JSON payload"
+            message = "Bad request; Ensure 'price', 'retailer_id' and 'name' are included in the JSON payload"
         )
     
     for item in items.values():
         if(
             item_data["name"] == item["name"]
-            and item_data["brand_id"] == item["brand_id"]
+            and item_data["retailer_id"] == item["retailer_id"]
         ):
             abort(400, message=f"Item already exists")
 
-    if item_data["brand_id"] not in brands:
-        abort(404, "Brand not found")
+    if item_data["retailer_id"] not in retailers:
+        abort(404, "retailer not found")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
     return item, 201
 
-# Retuns a specific brand
-@app.get("/brand/<string:brand_id>")
-def get_brand(brand_id):
+# Retuns a specific retailer
+@app.get("/retailer/<string:retailer_id>")
+def get_retailer(retailer_id):
     try:
-        return brands[brand_id]
+        return retailers[retailer_id]
     except KeyError:
-        abort(404, "Brand not found")
+        abort(404, "retailer not found")
+
+
+# Update specific item
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request; Ensure 'price', 'retailer_id' and 'name' are included in the JSON payload")
+    try:
+        item = items[item_id]
+        # pipe equals is the (new?) dictionary update operator
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found")
 
 # Returns a specific item
 @app.get("/item/<string:item_id>")
@@ -79,6 +95,7 @@ def get_item(item_id):
     except KeyError:
         abort(404, "Item not found")
 
+# Deletes item
 @app.delete("/item/<string:item_id>")
 def delete_item(item_id):
     try:
@@ -86,3 +103,12 @@ def delete_item(item_id):
         return {"message": "Item deleted"}
     except KeyError:
         abort(404, message="Item not found")
+
+# Deletes retailer
+@app.delete("/item/<string:retailer_id>")
+def delete_retailer(retailer_id):
+    try:
+        del retailers[retailer_id]
+        return {"message": "Retailer deleted"}
+    except KeyError:
+        abort(404, message="Retailer not found")
